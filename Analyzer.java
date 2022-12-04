531f657e737d84b7cb552f6999583fec04f30c56
tscott0817 <tylerscott.home@gmail.com>
2022-12-03 20:55:57 -0500

    Traceback finished

--- Analyzer.java
+++ Analyzer.java
@@ -2,78 +2,79 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class Analyzer {
 
-    List<Integer> M = new ArrayList<>();
-    List<Integer> originalList = new ArrayList<>();
+    List<Integer> lengths = new ArrayList<>();  // The array of lengths
+    List<Integer> originalList = new ArrayList<>();  // The list we're finding MIS of
+    List<Integer> tracebackResults = new ArrayList<>();  // The actual MIS
+
     public int analyze(List<Integer> list) {
 
-        // Copy list for class use and initialize memoized list
+        // Copy list for class use and initialize length list
         for (int i = 0; i < list.size(); i++) {
             originalList.add(list.get(i));
-            M.add(0);
+            lengths.add(0);
         }
-        M.set(M.size() - 1, 1);  // Set last element to length of 1
+        lengths.set(lengths.size() - 1, 1);  // Set last element to length of 1
 
-        // Iterate backwards from last element of list
+        // Iterate backwards starting from last element of list ss
         for (int i = list.size() - 1; i >= 0; i--) {
             opt(i);
-        }
-        //System.out.print(traceback(0));
 
-        return Collections.max(M);
+            // Print traceback results when at desired position
+            if (i == 0) {
+                System.out.print("Traceback Results: " + traceback(i));
+                System.out.print("\n");
+            }
+        }
+        return Collections.max(lengths);
     }
 
     public int opt(int i) {
 
-        int val = 0;
-        if (M.get(i) > 0) {
-            return M.get(i);
+        int maxLength = 0;
+        if (lengths.get(i) > 0) {
+            return lengths.get(i);
         }
         else {
             // Start at next position and check all elements until the end of list
             for (int j = i + 1; j < originalList.size(); j++) {
 
-                // Call recursively for each element that is larger than 'i'
+                // Recursively check for elements larger than current position
                 if (originalList.get(i) < originalList.get(j)) {
-                    val = 1 + opt(j);
-                    M.set(i, val);
+                    maxLength = 1 + opt(j);
+                    lengths.set(i, maxLength);
 
                     // Compare against max value and choose to continue or return
-                    if (val != Collections.max(M) & j < originalList.size() - 1) {
-                        val = opt(j + 1);
-
+                    if (maxLength != Collections.max(lengths) & j < originalList.size() - 1) {
+                        maxLength = opt(j + 1);
                     }
                     else {
-                        return val;
+                        return maxLength;
                     }
                 }
                 // If no larger elements to right, length is 1
                 else {
-                    val = 1;
-                    M.set(i, val);
+                    maxLength = 1;
+                    lengths.set(i, maxLength);
                 }
             }
-            return val;
-
+            return maxLength;
         }
     }
 
-    // TODO: The traceback function "unwinds" the computation of M[0] to show subsequence
     private List<Integer> traceback(int i) {
 
-        List<Integer> res = new ArrayList<>();
+        // Append new element to results after each recursive call
+        tracebackResults.add(originalList.get(i));
 
-        for (int j = 0; j < M.size(); j++) {
-            if (M.get(i) == 1 + M.get(j)) {
-
-
+        // Check each 'j' index to get list of max distance elements
+        for (int j = 0; j < originalList.size(); j++) {
+            if (lengths.get(i) == 1 + lengths.get(j) & j > i & tracebackResults.size() <= Collections.max(lengths) - 1) {
+                traceback(j);
             }
         }
-        return res;
-
+        return tracebackResults;
     }
-
 }
